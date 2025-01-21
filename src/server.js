90c81950eec6e18cfb13d3785227498b7f359a18
CallMeTrinity <anteur.ap@gmail.com>
2025-01-21 09:42:20 +0100

    fix : port from env file

--- src/server.js
+++ src/server.js
@@ -38,5 +38,5 @@ app.use('/api/answer', answerRoutes);
 initSockets(io);
 
 // Start the server
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
